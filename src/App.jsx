5d01cf2b7d29e41bcffd432c045c95d183bd9db3
Joanne Hoar <joanne.hoar@gmail.com>
2026-03-18 13:18:31 -0600

    turn lint error into warning (no unused vars)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import spaceImg from './assets/nasa.png'
 import './App.css'
 
 function App() {
   // todo: turn this into something useful
-  //  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
